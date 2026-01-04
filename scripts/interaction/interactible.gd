19046eff35b83b4687f36edad7d09aa576091a89
Steven McNeely <steven.mcneely@live.com>
2026-01-03 18:42:00 -0600

    Track interaction callables

--- scripts/interaction/interactible.gd
+++ scripts/interaction/interactible.gd
@@ -2,20 +2,27 @@ class_name Interactable extends Area2D
 
 signal on_interacted(interactor : Interactor)
 
+var _interact_callables : Dictionary = {}
+
 func _ready():
-	area_entered.connect(_on_area_entered)
-	area_exited.connect(_on_area_exited)
-	
+    area_entered.connect(_on_area_entered)
+    area_exited.connect(_on_area_exited)
+
 func _on_area_entered(area : Area2D):
-	if area is Interactor:
-		area.indicator.show()
-		area.on_interact.connect(_on_interact.bind(area))
-	
+    if area is Interactor:
+        area.indicator.show()
+        assert(not _interact_callables.has(area), "Interactor already connected")
+        var callable := Callable(self, "_on_interact").bind(area)
+        _interact_callables[area] = callable
+        area.on_interact.connect(callable)
+
 func _on_area_exited(area : Area2D):
-	if area is Interactor:
-		area.indicator.hide()
-		area.on_interact.disconnect(_on_interact)
+    if area is Interactor:
+        area.indicator.hide()
+        if _interact_callables.has(area):
+            area.on_interact.disconnect(_interact_callables[area])
+            _interact_callables.erase(area)
 
 func _on_interact(interactor : Interactor):
-	interactor.indicator.hide()
-	on_interacted.emit(interactor)
+    interactor.indicator.hide()
+    on_interacted.emit(interactor)
